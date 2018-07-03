82e9330cd6cf82f861ba850a24946bc82587c2bf
Oleksii Movchan <liohamitec@liohamitec.liohadomain>
2018-07-03 17:52:41 +0300

    first commit

---
+++ src/main/java/net/mysite/service/CustomerService.java
@@ -0,0 +1,16 @@
+package net.mysite.service;
+
+import net.mysite.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    Customer getById(Long id);
+
+    void save(Customer customer);
+
+    void delete(Long id);
+
+    List<Customer> getAll();
+}
